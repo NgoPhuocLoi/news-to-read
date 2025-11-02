3341bd69210986383f507d6ad56791d246e04da7
Ngo Phuoc Loi <phuocloi11223@gmail.com>
2025-11-02 20:40:16 +0700

    Update App metadata

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,10 +1,18 @@
 import { Button } from "@/components/retroui/Button";
+import Image from "next/image";
 import React from "react";
+import Logo from "../icon.png";
 
 const Header = () => {
   return (
     <div className="flex justify-between mb-6">
-      <p className="text-4xl font-sans font-bold mb-4">News to read</p>
+      <div className="flex gap-4 items-center">
+        <div className="rounded-full overflow-hidden">
+          <Image src={Logo} alt="Logo" width={50} height={50} />
+        </div>
+
+        <p className="text-4xl font-sans font-bold">News to read</p>
+      </div>
 
       <div className="flex gap-2 h-fit">
         <Button className="h-fit">Add bookmark</Button>
